61aa4fc57575e9399f51182b074587cf383cc1be
meltonzheng <63257392+meltonzheng@users.noreply.github.com>
2020-06-01 19:17:41 -0700

    Update mainwindow.h

--- mainwindow.h
+++ mainwindow.h
@@ -5,28 +5,21 @@
 #include <QPushButton>
 #include <QLabel>
 #include <QSpinBox>
-// arrange widgets vertically
 #include <QVBoxLayout>
 #include <QHBoxLayout>
 #include <QSlider>
-#include "window2.h"
 #include "receiver.h"
 #include <vector>
 
 class MainWindow : public QMainWindow
 {
-    Q_OBJECT // Macro, needed whenever your class uses signals and slots, goes in the class declaration
-    // signals and slots to be explained in a moment
-    // only works for subclasses of QObject
+    Q_OBJECT 
 
 public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
-public slots: // this is not standard C++ syntax
-    // Qt makes this work by providing its own
-    // preprocessor, which is why we need the
-    // Q_OBJECT macro above
+public slots: 
     void acceptInt(double);
 
 
